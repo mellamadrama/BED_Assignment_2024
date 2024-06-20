8f2598121e80e5e2c305211593885f0b2d1aaf61
rithika <133366256+sudarsanamrithika@users.noreply.github.com>
2024-06-20 17:11:09 +0800

    add validate challenge

---
+++ middlewares/validateChallenge.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+const validateChallenge = (req, res, next) => {
+  const schema = Joi.object({
+    ChallengeID: Joi.string().lenght(10).required(),
+    ChallengeDesc: Joi.string().min(1).max(200).required(),
+    Points: Joi.int().min(1).max(20).required(),
+  });
+
+  const validation = schema.validate(req.body, { abortEarly: false }); 
+
+  if (validation.error) {
+    const errors = validation.error.details.map((error) => error.message);
+    res.status(400).json({ message: "Validation error", errors });
+    return; 
+  }
+
+  next(); 
+};
+
+module.exports = validateBook;
