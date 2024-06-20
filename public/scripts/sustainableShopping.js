499a7f2b661ee3e7eb1815b2b39442702600f522
weiyingeve <smarteverest11@gmail.com>
2024-06-20 15:30:28 +0800

    added some scripting for sustainableShopping page

---
+++ public/scripts/sustainableShopping.js
@@ -0,0 +1,25 @@
+async function fetchLocations() {
+    const response = await fetch("/locations");
+    const data = await response.json();
+
+    const locationList = document.getElementById("location-list");
+
+    data.forEach((location) => {
+        const locationItem = document.createElement("div");
+        
+        const locationNameElement = document.createElement("h3");
+        locationNameElement.textContent = `Name: ${location.locationReqName}`;
+
+        const locationAddressElement = document.createElement("p");
+        locationAddressElement.textContent = `Address: ${location.locationReqAddress}`;
+
+        //add more elements
+
+        locationItem.appendChild(locationNameElement);
+        locationItem.appendChild(locationAddressElement);
+
+        locationList.appendChild(locationItem);
+    });
+}
+
+fetchLocations();
