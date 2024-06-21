ebb1496f76c6a367049d68bd2313bb87eecf7e14
weiyingeve <s10257093@connect.np.edu.sg>
2024-06-21 17:29:57 +0800

    edited adminLocationRequest.js to fetch the locations

---
+++ public/scripts/adminLocationRequest.js
@@ -0,0 +1,58 @@
+async function fetchLocationRequests() {
+    const response = await fetch("/locations");
+    const data = await response.json();
+
+    const locationList = document.getElementById("location-request-list");
+
+    data.forEach((location) => {
+        const locationItem = document.createElement("tr");
+        locationItem.classList.add("bg-[#908660]");
+        
+        const locationIdElement = document.createElement("td");
+        locationIdElement.textContent = location.locationReqId;
+        locationIdElement.classList.add("px-6 py-4 whitespace-nowrap text-sm text-gray-900");
+
+        const locationUserElement = document.createElement("td");
+        locationUserElement.textContent = location.userId;
+        locationUserElement.classList.add("px-6 py-4 whitespace-nowrap text-sm text-gray-900");
+
+        const locationContentElement = document.createElement("td");
+        locationContentElement.textContent = `${location.locationReqName}, ${location.locationReqAddress} `;
+        locationContentElement.classList.add("px-6 py-4 whitespace-nowrap text-sm text-gray-900");
+
+        const locationStatusElement = document.createElement("td");
+        const status = null;
+        switch (location.status) {
+            case 'A':
+                status = "Approved";
+                break;
+            case 'R':
+                status = "Rejected";
+                break;
+            case 'P':
+                status = "Pending";
+                break;
+            default:
+                status = "Pending";
+                break;
+        }
+        locationStatusElement.textContent = status;
+        locationStatusElement.classList.add("px-6 py-4 whitespace-nowrap text-sm text-gray-900");
+
+        const locationUpdateElement = document.createElement("td");
+        locationUpdateElement.createElement("a");
+        locationUpdateElement.href = `../adminApproveLocation.html`;
+        locationUpdateElement.textContent = `Update`;
+        locationUpdateElement.classList.add("px-6 py-4 whitespace-nowrap text-sm text-gray-900 underline");
+
+        locationItem.appendChild(locationIdElement);
+        locationItem.appendChild(locationUserElement);
+        locationItem.appendChild(locationContentElement);
+        locationItem.appendChild(locationStatusElement);
+        locationItem.appendChild(locationUpdateElement);
+
+        locationList.appendChild(locationItem);
+    });
+}
+
+fetchLocationRequests();
