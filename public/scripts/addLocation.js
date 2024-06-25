a3569e738ea2d30c52dec7b06c109ea4db379cd2
weiyingeve <s10257093@connect.np.edu.sg>
2024-06-25 14:10:58 +0800

    worked on addLocation.js file

---
+++ public/scripts/addLocation.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', async() => {
+    document.getElementById("requestLocationForm").addEventListener("submit", async function (e) {
+        e.preventDefault();
+    
+        const locationName = document.getElementById('locationName').value;
+        const locationAddress = document.getElementById('locationAddress').value;
+        //const user = 
+
+        if (locationName !='' && locationAddress !='') {
+            const locationDetails = {
+                locationReqName: locationName,
+                locationReqAddress: locationAddress,
+                status: 'P',
+                websiteLink: null,
+                //userId: user,
+                adminId: null
+            }
+    
+            const response = await fetch('/createLocations', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(locationDetails)
+            });
+    
+            const result = await response.json();
+    
+            if (!response.ok) {
+                alert(`Failed to send location request: ${result.locationReqName}`);
+                return;
+            }
+    
+            if (result.status === 'success') {
+                alert('Location Request submitted!');
+    
+                //reset form fields after successful submission
+                locationName = '';
+                locationAddress = '';
+            } else {
+                alert('Failed to submit location request');
+            }
+        } else {
+            alert('Location Name and Address cannot be empty!');
+        }
+    })
+})
