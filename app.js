d255bf75a3f5a1836e8fe73cc3df7536fe80f0a8
mellamadrama <s10258027@connect.np.edu.sg>
2024-07-28 22:43:31 +0800

    fixed: descriptions for swagger
    Co-authored-by: rithika <sudarsanamrithika@users.noreply.github.com>
    Co-authored-by: weiyingeve <weiyingeve@users.noreply.github.com>
    Co-authored-by: Rena <xenoroxy@users.noreply.github.com>

--- app.js
+++ app.js
@@ -54,26 +54,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticMiddleware);
 
-//login
+//login done by Rena (S10258053C)
 app.post('/login', validateLogin, loginController.loginUser);
 app.post('/loginAdmin', validateAdminLogin, adminLoginController.loginAdmin);
 
-//signup
+//signup done by Rena (S10258053C)
 app.post('/signup', validateUserSignup, userSignupController.createUserAccount);
 app.post('/adminsignup', validateAdminSignup, adminSignupController.createAdminAccount);
 
-// categories done by charlotte (S10258027)
+// categories done by Charlotte (S10258027K)
 app.get("/categories", verifyJWT, categoryController.getAllCategories);
 app.get('/categories/:catId', verifyJWT, categoryController.getCategoryById);
 
-// weeks done by charlotte (S10258027)
+// weeks done by Charlotte (S10258027K)
 app.get("/weeks", verifyJWT, weekController.getAllWeeks);
 app.get("/weeks/:catId/:userId", verifyJWT, weekController.getWeekByUserCatId);
 app.post("/weeks", verifyJWT, validateWeek, weekController.createWeek);
 app.put("/weeks/:weekName/:catId/:userId", verifyJWT, validateUpdateWeekName, weekController.updateWeekAndData);
 app.delete('/weeks/:weekName/:catId/:userId', verifyJWT, weekController.deleteWeekAndData);
 
-// datainput done by charlotte (S10258027)
+// datainput done by Charlotte (S10258027K)
 app.get("/datainput", verifyJWT, dataInputController.getAllCatDataInput);
 app.get("/datainput/:weekName/:catId/:userId/:dataId", verifyJWT, dataInputController.getCatDataInputByIds);
 app.get("/datainput/:weekName/:catId/:userId", verifyJWT, dataInputController.getCatDataInputById);
@@ -81,7 +81,7 @@ app.post("/datainput", verifyJWT, validateDataInput, dataInputController.createD
 app.put("/datainput/:weekName/:catId/:userId/:dataId", verifyJWT, validateDataInputs, dataInputController.updateCatDataInput);
 app.delete('/datainput/:weekName/:catId/:userId/:dataId', verifyJWT, dataInputController.deleteCatDataInput);
 
-//chat bot done by charlotte (S10258027)
+//chat bot done by Charlotte (S10258027K)
 app.get("/chathistory/:userId", verifyJWT, chatHistoryController.getChatByUserId);
 app.post("/chathistory", verifyJWT, validateSaveChat, chatHistoryController.saveChat);
 app.post('/generate-text', verifyJWT, chatHistoryController.generateGeminiResponse);
@@ -89,33 +89,33 @@ app.post('/generate-text', verifyJWT, chatHistoryController.generateGeminiRespon
 // Serve the Swagger UI at a specific route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// locations
+// locations done by Isabelle (S10257093F)
 app.get("/locations", verifyJWT, locationController.getAllLocations);
 app.get("/locations/:locationReqId", verifyJWT, locationController.getLocationById);
 app.post("/createlocations", verifyJWT, validateLocation, locationController.createLocation);
 app.put("/updlocations/:locationReqId", verifyJWT, validateLocation, locationController.updateLocation);
 app.delete("/dellocations/:locationReqId", verifyJWT, locationController.deleteLocation);
 
-//events
+//events done by Isabelle (S10257093F)
 app.get("/events", verifyJWT, eventController.getAllEvents);
 app.get("/events/:eventId", verifyJWT, eventController.getEventById);
 app.post("/createevents", verifyJWT, validateEvent, eventController.createEvent);
 app.put("/updateevents/:eventId", verifyJWT, validateEvent, eventController.updateEvent);
 app.delete("/deleteevents/:eventId", verifyJWT, eventController.deleteEvent);
 
-// challenges
+// challenges done by Rithika (S10257149F)
 app.get("/challenges", verifyJWT, challengeController.getAllChallenges);
 app.get("/challenges/:id", verifyJWT, challengeController.getChallengeByID);
 app.post("/createchallenges", verifyJWT, validateChallenge, challengeController.createChallenge);
 app.delete("/challenges/:id", verifyJWT, challengeController.deleteChallenge);
 
-// user weekly challenges
+// user weekly challenges done by Rithika (S10257149F)
 app.get("/userchallenges/:userId", verifyJWT, userWeeklyChallengeController.getAllChallengesByUserID);
 app.put("/updateuserchallenges/:challengeId/:userId", verifyJWT, userWeeklyChallengeController.updateChallengeCompleted);
 app.delete("/deleteuserchallenges/:challengeId", verifyJWT, userWeeklyChallengeController.deleteChallengeForEachUser);
 app.post("/createuserchallenges", verifyJWT, userWeeklyChallengeController.createChallengeForEachUser);
 
-// points
+// points done by Rithika (S10257149F)
 app.get("/weeklypoints", verifyJWT, weeklyPointsController.getAllWeeklyPoints);
 app.get("/monthlypoints", verifyJWT, monthlyPointsController.getAllMonthlyPoints);
 app.put("/resetweekly", verifyJWT, weeklyPointsController.resetWeeklyPoints);
@@ -125,19 +125,19 @@ app.put("/addmonthly/:userId", verifyJWT, monthlyPointsController.addPointsToMon
 app.get("/userweeklypoints/:userId", verifyJWT, weeklyPointsController.getUserWeeklyPoints);
 app.get("/usermonthlypoints/:userId", verifyJWT, monthlyPointsController.getUserMonthlyPoints);
 
-// user account
+// user account done by Rena (S10258053C)
 app.get("/getuser", verifyJWT, userAccController.getAllUsers);
 app.get("/getuser/:userId", verifyJWT, userAccController.getAllUsersById);
 app.put("/updateuser/:userId", verifyJWT, userAccController.updateUserAccount);
 app.delete("/deleteuser/:userId", verifyJWT, userAccController.deleteUserAccount);
 
-// user history (request location)
+// user history (request location) done by Rena (S10258053C)
 app.get("/userlocations", verifyJWT, userLocationController.getAllLocations);
 app.get("/userlocation/:userId", verifyJWT, userLocationController.getLocationByUserId);
 app.get("/usertracker", verifyJWT, userTrackerController.getAllCategories)
 app.get("/usertracker/:userId", verifyJWT, userTrackerController.getCatDataInputByUserId)
 
-// admin account
+// admin account done by Rena (S10258053C)
 app.get("/getadmin", verifyJWT, adminAccountController.getAllAdmins);
 app.get("/getadmin/:adminId", verifyJWT, adminAccountController.getAllAdminsById);
 app.put("/updateadmin/:adminId", verifyJWT, adminAccountController.updateAdminAccount);
